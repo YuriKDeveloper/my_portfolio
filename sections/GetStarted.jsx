95040c799699208476ce879b94a0db73c79eb6a9
Yuri <yurioichikts@gmail.com>
2023-03-15 19:41:29 -0300

    novo commit

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -7,34 +7,101 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const GetStarted = () => {
   const projects = [
     {
-      title: "ABC mais telecom",
-      description: "Design & Development",
+      title: 'ABC mais telecom',
+      description: 'Design & Development',
       url: './p1.png',
+      urlLink: 'https://abcmaistelecom.com.br',
     },
     {
-      title: "Valorise",
-      description: "Design & Development",
+      title: 'Valorise',
+      description: 'Design & Development',
       url: './p2.png',
     },
     {
-      title: "Max detox",
-      description: "Design & Development",
+      title: 'Max detox',
+      description: 'Design & Development',
       url: './p3.png',
     },
     {
-      title: "Flua contabilidade",
-      description: "Design & Development",
+      title: 'Flua contabilidade',
+      description: 'Design & Development',
       url: './4.png',
     },
     {
-      title: "Move auto peças",
-      description: "Design & Development",
+      title: 'Move auto peças',
+      description: 'Design & Development',
       url: './5.png',
     },
     {
-      title: "Paix",
-      description: "Design & Development",
+      title: 'Paix',
+      description: 'Design & Development',
       url: './6.png',
+    },
+  ];
+
+  const projects2 = [
+    {
+      title: 'Concurseiro fora da curva',
+      description: 'Design & Development',
+      url: './7.png',
+    },
+    {
+      title: 'Contabilidade Pelanda',
+      description: 'Design & Development',
+      url: './8.png',
+    },
+    {
+      title: 'Oficina Qualitat',
+      description: 'Design & Development',
+      url: './9.png',
+    },
+    {
+      title: 'Lugit',
+      description: 'Design & Development',
+      url: './10.png',
+    },
+    {
+      title: 'Meta ID Trade',
+      description: 'Design & Development',
+      url: './11.png',
+    },
+    {
+      title: 'AFP Contabilidade',
+      description: 'Design & Development',
+      url: './12.png',
+    },
+  ];
+
+  const projects3 = [
+    {
+      title: 'ABC mais telecom',
+      description: 'Design & Development',
+      url: './13.png',
+    },
+    {
+      title: 'Valorise',
+      description: 'Design & Development',
+      url: './14.png',
+    },
+    {
+      title: 'Max detox',
+      description: 'Design & Development',
+      url: './15.png',
+    },
+    {
+      title: 'Flua contabilidade',
+      description: 'Design & Development',
+      url: './16.png',
+    },
+    {
+      title: 'Move auto peças',
+      description: 'Design & Development',
+      url: './17.png',
+    },
+    {
+      title: 'Paix',
+      description: 'Design & Development',
+      url: './18.png',
     },
   ];
   return (
@@ -42,11 +109,11 @@ const GetStarted = () => {
       <Container>
         <Row>
           <Col size={12}>
-              <div>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+            <div>
+              <h2>Projetos</h2>
+              <p>Alguns dos projetos que desenvolvi durante minha carreira espero que apreciem.</p>
+              <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
                     </Nav.Item>
@@ -57,7 +124,7 @@ const GetStarted = () => {
                       <Nav.Link eventKey="third">Tab 3</Nav.Link>
                     </Nav.Item>
                   </Nav>
-                  <Tab.Content id="slideInUp">
+                <Tab.Content id="slideInUp">
                     <Tab.Pane eventKey="first">
                       <Row>
                         {
@@ -69,16 +136,34 @@ const GetStarted = () => {
                           ))
                         }
                       </Row>
-                      </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <Row>
+                        {
+                          projects2.map((project, index) => (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          ))
+                        }
+                      </Row>
                     </Tab.Pane>
                     <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
+                      <Row>
+                        {
+                          projects3.map((project, index) => (
+                            <ProjectCard
+                              key={index}
+                              {...project}
+                            />
+                          ))
+                        }
+                      </Row>
                     </Tab.Pane>
                   </Tab.Content>
-                </Tab.Container>
-              </div>
+              </Tab.Container>
+            </div>
           </Col>
         </Row>
       </Container>
--- sections/projectcard.jsx
+++ sections/projectcard.jsx
@@ -2,15 +2,17 @@
 
 import { Col } from 'react-bootstrap';
 
-export const ProjectCard = ({ title, description, url }) => (
+export const ProjectCard = ({ title, description, url, urlLink }) => (
   <Col size={12} sm={6} md={4}>
     <div className="proj-imgbx">
+      <a href={urlLink}>
         <img src={url} />
         <div className="proj-txtx">
           <h4>{title}</h4>
           <span>{description}</span>
         </div>
-      </div>
+        </a>
+    </div>
   </Col>
 );
 
