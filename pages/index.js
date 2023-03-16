df2bc729cf56d28aa6201a45576f5da2f47eabdb
Yuri Laurindo <94491525+YuriKDeveloper@users.noreply.github.com>
2023-03-15 21:45:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -12,8 +12,8 @@ const Home = () => (
     </div>
     <div className="relative">
       <GetStarted />
-      <div className="gradient-04 z-0" />
       <WhatsNew />
+    <div className="gradient-04 z-0" />
     </div>
     <World />
     <div className="relative">
