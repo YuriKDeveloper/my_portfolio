3372535bcc9144a5b5fc56e817cbc3a3b76e4e84
Yuri Laurindo <94491525+YuriKDeveloper@users.noreply.github.com>
2023-03-17 15:24:35 -0300

    Update GetStarted.jsx

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -132,13 +132,13 @@ const GetStarted = () => {
               <Tab.Container id="projects-tabs" defaultActiveKey="first">
                 <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
-                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                      <Nav.Link eventKey="first">Projetos</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                      <Nav.Link eventKey="second">Projetos</Nav.Link>
                     </Nav.Item>
                     <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                      <Nav.Link eventKey="third">Projetos</Nav.Link>
                     </Nav.Item>
                   </Nav>
                 <Tab.Content id="slideInUp">
