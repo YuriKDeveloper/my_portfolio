ac758167342b64edfe7ff47f56b80f1ddd150e79
Yuri Laurindo <94491525+YuriKDeveloper@users.noreply.github.com>
2023-03-17 15:23:41 -0300

    Update GetStarted.jsx

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -8,100 +8,117 @@ const GetStarted = () => {
   const projects = [
     {
       title: 'ABC mais telecom',
-      description: 'Design & Development',
+      description: 'Locação de rádios',
       url: './p1.png',
       urlLink: 'https://abcmaistelecom.com.br',
     },
     {
       title: 'Valorise',
-      description: 'Design & Development',
+      description: 'Crescimento empresarial',
       url: './p2.png',
+      urlLink: 'https://valorise.com.br',
     },
     {
       title: 'Max detox',
-      description: 'Design & Development',
+      description: 'Produtos para emagrecimento',
       url: './p3.png',
+      urlLink: 'https://lojamaxdetox.com.br',
     },
     {
       title: 'Flua contabilidade',
-      description: 'Design & Development',
+      description: 'Contabilidade',
       url: './4.png',
+      urlLink: 'https://fluacontabilidade.com.br',
     },
     {
       title: 'Move auto peças',
-      description: 'Design & Development',
+      description: 'Venda de produtos para carro',
       url: './5.png',
+      urlLink: 'https://moveautopecas.com.br',
     },
     {
       title: 'Paix',
-      description: 'Design & Development',
+      description: 'Tecnologia',
       url: './6.png',
+      urlLink: 'http://paix.eng.br',
     },
   ];
 
   const projects2 = [
     {
       title: 'Concurseiro fora da curva',
-      description: 'Design & Development',
+      description: 'Site de cursos',
       url: './7.png',
+      urlLink: 'https://concurseiroforadacurva.com.br',
     },
     {
       title: 'Contabilidade Pelanda',
-      description: 'Design & Development',
+      description: 'Contabilidade',
       url: './8.png',
+      urlLink: 'https://contabilidadepelanda.com.br',
     },
     {
       title: 'Oficina Qualitat',
       description: 'Design & Development',
       url: './9.png',
+      urlLink: 'https://oficinaqualitat.com.br',
     },
     {
       title: 'Lugit',
-      description: 'Design & Development',
+      description: 'Loja de roupas',
       url: './10.png',
+      urlLink: 'https://lugit.com.br',
     },
     {
       title: 'Meta ID Trade',
-      description: 'Design & Development',
+      description: 'Importação de produtos',
       url: './11.png',
+      urlLink: 'https://metaidtrade.com.br',
     },
     {
       title: 'AFP Contabilidade',
-      description: 'Design & Development',
+      description: 'Contabilidade',
       url: './12.png',
+      urlLink: 'https://afpcontabilidade.com.br',
     },
   ];
 
   const projects3 = [
     {
-      title: 'ABC mais telecom',
-      description: 'Design & Development',
+      title: 'Xjur',
+      description: 'Software juridico',
       url: './13.png',
+      urlLink: 'https://xjur.com.br',
     },
     {
-      title: 'Valorise',
-      description: 'Design & Development',
+      title: 'Br Brands',
+      description: 'Importação de produtos',
       url: './14.png',
+      urlLink: 'https://brbrandsangola.com.br',
     },
     {
-      title: 'Max detox',
-      description: 'Design & Development',
+      title: 'Previne Angola',
+      description: 'Site de detetização',
       url: './15.png',
+      urlLink: 'https://previneangola.com.br',
     },
     {
-      title: 'Flua contabilidade',
-      description: 'Design & Development',
+      title: 'Z&Z Store',
+      description: 'Aluguel de vestidos',
       url: './16.png',
+      urlLink: 'https://zezstore.com.br',
     },
     {
-      title: 'Move auto peças',
-      description: 'Design & Development',
+      title: 'ZapNaWeb',
+      description: 'Software comunicação',
       url: './17.png',
+      urlLink: 'https://zapnaweb.com.br',
     },
     {
-      title: 'Paix',
-      description: 'Design & Development',
+      title: 'Myse API Faznota',
+      description: 'WB api faznota',
       url: './18.png',
+      urLink: 'https://www.myse.com.br/api-faznota.php',
     },
   ];
   return (
